70b1dec9c39271738acbc43cf6c0264af946ca39
Sharkov Ivan <sharkov.agent@mail.ru>
2023-01-11 22:49:28 +0300

    Fix uploads url

--- utils/multer.js
+++ utils/multer.js
@@ -11,15 +11,15 @@ import checkAuth from "./checkAuth.js";
 
 export const multerUploads = () => {
   // -- Доступ к картинке
-  app.use("/uploads", express.static("uploads")); // -- GET запрос на получение статичного файла по его названию (с расширением)
+  app.use("/uploads", express.static("../uploads")); // -- GET запрос на получение статичного файла по его названию (с расширением)
 
   // -- Multer storage
   const storage = multer.diskStorage({
     destination: (__, _, cb) => {
-      if (!fs.existsSync("uploads")) {
-        fs.mkdirSync("uploads");
+      if (!fs.existsSync("../uploads")) {
+        fs.mkdirSync("../uploads");
       }
-      cb(null, "uploads");
+      cb(null, "../uploads");
     },
     filename: (_, file, cb) => {
       cb(null, file.originalname);
